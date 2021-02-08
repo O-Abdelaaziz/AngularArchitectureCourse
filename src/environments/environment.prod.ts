7e90ba032c1dfc8916f03a1ddc416b2c09708cd6
Ouakala Abdelaaziz <mr.ouakala.abdelaaziz@gmail.com>
2021-02-08 14:02:11 +0100

    add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,14 @@
 export const environment = {
   production: true,
-  name:'prod'
+  name:'prod',
+  firebase:{
+    config:{
+        apiKey: 'AIzaSyCIsGoyMtr8O04NydYrwd8ChyndEYB5j5A',
+        authDomain: 'angulararchitecturecours-f07e7.firebaseapp.com',
+        projectId: 'angulararchitecturecours-f07e7',
+        storageBucket: 'angulararchitecturecours-f07e7.appspot.com',
+        messagingSenderId: '1086597250880',
+        appId: '1:1086597250880:web:413c16beeae803df76c4a5'
+    }
+}
 };
